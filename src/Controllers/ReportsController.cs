c95c87f0d0619a6cc2b7a20bdbc732bc9ffda524
Miroslav Novta <miroslav.novta@gmail.com>
2020-11-22 16:35:48 +0100

    report is updated

--- src/Controllers/ReportsController.cs
+++ src/Controllers/ReportsController.cs
@@ -3,6 +3,7 @@ using GoldenTicket.Models.ReportsViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace GoldenTicket.Controllers
 {
@@ -30,26 +31,9 @@ namespace GoldenTicket.Controllers
         [HttpGet]
         public async Task<IActionResult> All()
         {
-            // var timeSpan = new TimeSpan(hours: 24, minutes: 0, seconds: 0);
-            // var end = DateTime.Now;
-            // var start = end - timeSpan;
-
-            // var openEvents = (await _context.Tickets.Where(ticket => ticket.DateAdded > start || (ticket.DateAdded <= start && (ticket.DateClosed > start || ticket.Open))).Select(ticket => ticket.DateAdded < start ? start : ticket.DateAdded).ToListAsync()).Select(time => (time, true));
-            // var closeEvents = (await _context.Tickets.Where(ticket => ticket.DateClosed > start).Select(ticket => ticket.DateClosed).ToListAsync()).Select(time => (time, false));
-
-            // var events = new List<IEnumerable<(DateTime, bool)>> { openEvents, closeEvents }.SelectMany(ticket => ticket); OrderBy<IQueryable<(DateTime, bool)>, bool>((time, open) => true);
-
             var details = new ReportDetails
             {
-                //AverageQueueLength = await _context.Tickets.Where(ticket => ticket.Open).CountAsync(),
-                //AverageWait = new TimeSpan(0, 0, 0),
-                //EmptyQueuePercentage = 0,
-                //TicketsNotAddressedSameDay = 15,
-                //ModeratorReviews = await _context.Users.GroupJoin(_context.TicketReviews,
-                //                                                clinet => clinet.Id,
-                //                                                review => review.ModeratorId,
-                //                                                (clinet, review) => new { Moderator = clinet, Outcome = review})
-                //                                        .ToAsyncEnumerable().Select(x => (x.Moderator, x.Outcome)).ToList()
+                Reviews = new List<(Models.Client Client, Models.Review? ReviewOutcome)>(),
             };
             return View(details);
         }
